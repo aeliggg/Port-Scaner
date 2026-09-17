fb67528edb108adf5346e6dd9dbea5e79bce209c
aeliggg <aelig.hamadou@gmail.com>
2026-09-17 17:01:56 -0400

    test with port 8000 with py serv , good

--- scaner.py
+++ scaner.py
@@ -1,14 +1,38 @@
 import socket
 
 class Port :
-    type = ["Open", "Closed", "filtered"]
-    def __init__ (self, name):
+    type = ""
+    id:int
+    def __init__ (self, name,id):
         self.name=name
+        self.id=id
 
-def scan (ip,port):
+def scan (ip,port:Port):
+    idPort=port.id
+    Filtered=False
     timeout = 1
-    socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    socket.connect_ex()
+    socketScan=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    socketScan.settimeout(timeout)
+    try :
+        response = socketScan.connect_ex((ip,idPort))
+    except socket.timeout:
+        port.type="filtered"
+        Filtered=True
+        ##socketScan.close()
+        ##return()
+    if Filtered==False :
+        if (response==0) :
+            port.type="Open"
+        else :
+            port.type="Closed"
+    socketScan.close()
+
+testPort=Port("test",8000)
+scan("127.0.0.1", testPort)
+print(testPort.type)
+
+
+    
 
 
 
